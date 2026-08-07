49059781fc2165642a57562afb4f16ca7b19a185
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-08-07 23:06:44 +0300

    update Target SDK to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.nicos.liveupdatenotification"
         minSdk = 29
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
