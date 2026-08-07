66d45bcfdb90f808dcde9ba0127a8b44b2a6ff97
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-08-07 23:06:32 +0300

    update Target SDK to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.nicos.liveupdatenotification"
-    buildToolsVersion = "36.0.0"
+    buildToolsVersion = "37.0.0"
     compileSdk = 36
 
     defaultConfig {
